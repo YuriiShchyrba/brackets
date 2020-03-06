9288b14cf73d8bd2087b191cf587669e6de096f1
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-03-06 03:03:35 -0500

    sss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var sum = 0;
+    if (braces[0] == "}" || braces[0] == "]" || braces[0] == ")") return false;
+    var map = { "{": "}", "[": "]", "(": ")" };
+    for (var i = 0; i < braces.length; i++) {
+        var q = map[arr[arr.length - 1]];
+        var qq = braces[i];
+        if (braces[i] == "(") {
+            sum++;
+        }
+        else if (braces[i] ==")") {
+            sum--;
+        }
+    }
+
+    if (sum == 0) return true;
+
+    return false;
 }
