6ec3385f7204493b780a35486a87d64b90bd7ad1
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-03-06 03:11:05 -0500

    dsdsd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function check(braces, bracketsConfig) {
   var arr = [];
   if (braces[0] == "}" || braces[0] == "]" || braces[0] == ")") return false;
-  var map = { "{": "}", "[": "]", "(": ")" };
+  var map = { "{": "}", "[": "]", "(": ")" , "|":"|" };
   for (var i = 0; i < braces.length; i++) {
       var q = map[arr[arr.length - 1]];
       var qq = braces[i];
-      if (braces[i] == "{" || braces[i] == "[" || braces[i] == "(") {
+      if (braces[i] == "{" || braces[i] == "[" || braces[i] == "(" || braces[i] == "|") {
           arr.push(braces[i]);
       }
       else if (map[arr[arr.length - 1]] == braces[i]) {
