ece7e067352708563a338792570c410ad4b19fc3
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-03-06 03:05:50 -0500

    dsdsd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 module.exports = function check(braces, bracketsConfig) {
-  var sum = 0;
-    if (braces[0] == "}" || braces[0] == "]" || braces[0] == ")") return false;
-    var map = { "{": "}", "[": "]", "(": ")" };
-    for (var i = 0; i < braces.length; i++) {
-        var q = map[arr[arr.length - 1]];
-        var qq = braces[i];
-        if (braces[i] == "(") {
-            sum++;
-        }
-        else if (braces[i] ==")") {
-            sum--;
-        }
-    }
+  var arr = [];
+  if (braces[0] == "}" || braces[0] == "]" || braces[0] == ")") return false;
+  var map = { "{": "}", "[": "]", "(": ")" };
+  for (var i = 0; i < braces.length; i++) {
+      var q = map[arr[arr.length - 1]];
+      var qq = braces[i];
+      if (braces[i] == "{" || braces[i] == "[" || braces[i] == "(") {
+          arr.push(braces[i]);
+      }
+      else if (map[arr[arr.length - 1]] == braces[i]) {
+          arr.pop();
+      }
+      else {
+          arr.push(braces[i]);
+      }
 
-    if (sum == 0) return true;
+  }
 
-    return false;
+  if (arr.length == 0) return true;
+
+  return false;
 }
