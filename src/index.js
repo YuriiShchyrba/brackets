f0d996a22b5a8dd4d7b85d670970b1ab2e57ce04
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-03-06 13:00:05 -0500

    dsdsd

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 module.exports = function check(braces, bracketsConfig) {
   var arr = [];
-  var config = new Map(bracketsConfig);
+    var config = new Map(bracketsConfig);
 
-  for ( var start of config.values())
-  {
-    if ( braces[0] == start)
-    {
-      return false;
+    for (var start of config.values()) {
+        if (braces[0] == start && braces[0]!="|" && braces[0] != "7" && braces[0] !="8") {
+            return false;
+        }
     }
-  }
 
-  for ( var i = 0 ; i < braces.length; i++)
-  {
-    if ( config.has(braces[i]))
-    {
-      arr.push(braces[i]);
-    }
-    else if ( braces[i] == config.get(arr[arr.length-1]))
-    {
-      arr.pop();
-    }
-    else{
-      return false;
-    }
-  }
 
-  if (arr.length == 0) return true;
+    var check = true;
+    for (var i = 0; i < braces.length; i++) {
 
-  return false;
+        if (config.has(braces[i])) {
+            if (braces[i]== config.get(braces[i]) && check) {
+                arr.push(braces[i]);
+                check = false;
+            }
+            else if (braces[i] != config.get(braces[i]))  {
+                arr.push(braces[i]);
+            }
+            
+        }
+       if (braces[i] == config.get(arr[arr.length - 1])) {
+
+            for (var k in config.keys()) {
+                if (k == braces[i] && !check) {
+                    check = true;
+                }
+            }
+
+            arr.pop();
+            
+        }
+    }
+
+    if (arr.length == 0) return true;
+
+    return false;
 }
